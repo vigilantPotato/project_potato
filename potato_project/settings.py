0e5397e4d140379aedf28a7b3b1b338a2328c3fe
vigilantPotato <vigilant.potato@gmail.com>
2018-12-17 19:49:37 +0900

    add whitenoise

--- potato_project/settings.py
+++ potato_project/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'potato_project.urls'
@@ -120,3 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STRAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
